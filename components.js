09b40ff8704903605b98357520fb3edf561efa7d
Cyril Bourdon <cyril.bourdon@hotmail.fr>
2022-07-18 22:55:06 +0200

    Ajout screenshot

---
+++ components.js
@@ -0,0 +1,11 @@
+/*********  Ajout Audio *********/
+
+const BG_GAME = new Audio(src = 'audio/BG.OGG');
+BG_GAME.volume = 0.5;
+const WALL_COLLISION = new Audio(src = 'audio/collisionWall.wav');
+const GOAL_P1 = new Audio(src = 'audio/goalP1.mp3');
+const GOAL_P2 = new Audio(src = 'audio/goalP2.mp3');
+const PADDLE_P1_COLLISION = new Audio(src = 'audio/paddleP1.ogg');
+const PADDLE_P2_COLLISION = new Audio(src = 'audio/paddleP2.ogg');
+const START_GAME = new Audio(src = 'audio/but.wav');
+const GAME_OVER = new Audio(src = 'audio/applause.wav');
